0739624aca166408b7bd58f77731209e9f1c72b8
masaya2983 <127204441+masaya2983@users.noreply.github.com>
2023-12-11 21:28:49 +0900

    コメント

---
+++ 5章/5-3/no_existence.js
@@ -0,0 +1,4 @@
+const interests = ['読書','料理','キャンプ'];
+const element5 = interests[5];
+console.log(element5);
+
---
+++ 5章/5-4/update_element.js
@@ -0,0 +1,3 @@
+const interests = ['読書','料理','キャンプ'];
+const element0 = '散歩';
+console.log(interests);
